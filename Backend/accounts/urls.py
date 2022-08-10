0c53fe17a9f636605f75bb286f584920ef70d0eb
Akinjide <jideoscar88@gmail.com>
2022-08-10 17:39:46 +0100

    User Authentication

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -4,6 +4,10 @@ from django.urls import path
 app_name = 'accounts' 
 urlpatterns = [        
     path('',views.index, name='index'),
-    path('register', views.register, name='register'),       
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('resetpassword/', views.reset, name='resetpassword'),
+    path('forgotpassword/', views.forgot, name='forgetpassword'),       
     path('contact', views.contact, name='contact'),
 ] 
--- Backend/accounts/views.py
+++ Backend/accounts/views.py
@@ -6,6 +6,12 @@ from django.core.paginator import Paginator
 from django.contrib import messages
 from .forms import RegisterForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_text
+from . tokens import generate_token
 
 
 # Create your views here.
@@ -28,26 +34,86 @@ def index(request):
     return render(request, 'accounts/index.html', context) 
 
 
-# Registration view
+#Registration page
 def register(request):
-    if request.user.is_authenticated:
-        return redirect("/")
-    if request.method == 'POST': 
-        form = RegisterForm(request.POST) 
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            email = form.cleaned_data.get('email')
-            if User.objects.filter(username=username).exists():
-                messages.error(request, f'This username {username} already exists!')
-            if User.objects.filter(email=email).exists():
-                messages.error(request, f'The email {email} already exists!')
-                return redirect('accounts:register') 
-            form.save()                              
-            messages.success(request, f'Welcome {username}, your account is created.')            
-            return redirect('login')
-    else:
-        form = RegisterForm()
-    return render(request, 'accounts/registration.html', {'form':form})
+
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if User.objects.filter(email = email):
+            messages.error(request, "Email already register")
+            return redirect('register')
+
+        # if User.objects.filter(email = email):
+        #     messages.error(return redirect('login')quest, "Email already exist")
+        
+        myuser = User.objects.create_user(username, email, password)
+        myuser.is_active = False
+        myuser.save()
+
+        messages.success(request, "Your Account has been successfully created. We have sent you a confirmation email")
+
+        #Email Address Confirmation Email
+
+        current_site = get_current_site(request)
+        email_subject = "Confirm your email @ accounts - Django Login!!"
+        message2 = render_to_string("email_confirmation.html"),{
+            'name': username,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser)
+        }
+        email = EmailMessage(
+            email_subject,
+            message2,
+            settings.EMAIL_HOST_USER,
+            [myuser.email],
+        )
+        email.fail_silently = True
+        email.send()
+
+        return redirect('login')
+
+    return render(request, "accounts/registration.html")
+
+#Login page
+def login(request):
+
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = authenticate(email = email, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect(request, "accounts/homepage.html")
+
+        else:
+            messages.error(request, "Incorrect email or password.")
+            return redirect(request, "accounts/login.html")
+
+    return render(request, "accounts/login.html")
+
+#Logout page
+def logout(request):
+    logout(request, user)
+    messages.success(request, "Logged out Successfully")
+    return render(request, "accounts/index.html")
+
+#Reset password page
+def reset(request):
+    return render(request, "accounts/resetpassword.html")
+
+#Forgot password page
+def forgot(request):
+    return render(request, "accounts/forgotpassword.html")
+
+#Login page
+# def login(request):
+#     return render(request, "accounts/login.html")
 
 
 # Profile page view
